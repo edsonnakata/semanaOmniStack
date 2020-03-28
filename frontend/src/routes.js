8638587680e0e794a42ab7cf55afaf279d415164
enakata <edson.nakata@webtechbr.com>
2020-03-28 12:04:53 -0400

    Finalizaçao da pagina de profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 /**
  * Precisa exportar as rotas
@@ -24,6 +25,7 @@ export default function Routes() {
       <Switch>
        <Route path="/" exact component={Logon} />
        <Route path="/register" component={Register} />
+       <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
