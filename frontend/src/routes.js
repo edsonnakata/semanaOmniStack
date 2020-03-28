5031c3c747b7a23a4632a68d93c246fde6e09af9
enakata <edson.nakata@webtechbr.com>
2020-03-28 12:43:10 -0400

    Finalizado a paina de registrar novos casos

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident'
 
 /**
  * Precisa exportar as rotas
@@ -26,6 +27,7 @@ export default function Routes() {
        <Route path="/" exact component={Logon} />
        <Route path="/register" component={Register} />
        <Route path="/profile" component={Profile} />
+       <Route path="/incidents/new" component={NewIncident}/>
       </Switch>
     </BrowserRouter>
   );
